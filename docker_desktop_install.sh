1904cd9bd87f15ddb44b5c76e80525607fc2ff13
anshit-1223 <anshit1223@gmail.com>
2025-08-14 12:00:00 +0530

    Commit for 2025-08-14

--- docker_desktop_install.sh
+++ docker_desktop_install.sh
@@ -7,7 +7,7 @@ if [ $check_root -ne 0 ]; then
 fi
 
 echo "Download Docker Desktop"
-#wget -O docker-desktop-amd64.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64&_gl=1*mega3h*_ga*NDg0OTU2MDU0LjE3NTk2NzIyMjU.*_ga_XJWPQMJYHQ*czE3NjkyNjc5MjgkbzUkZzEkdDE3NjkyNjc5NDQkajQ0JGwwJGgw"; 
+wget -O docker-desktop-amd64.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64&_gl=1*mega3h*_ga*NDg0OTU2MDU0LjE3NTk2NzIyMjU.*_ga_XJWPQMJYHQ*czE3NjkyNjc5MjgkbzUkZzEkdDE3NjkyNjc5NDQkajQ0JGwwJGgw"; 
 echo "Installing required dependencies"
 sudo apt update
 sudo apt install -y qemu-system-x86 pass uidmap
