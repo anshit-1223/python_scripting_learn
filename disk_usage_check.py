d2656e858f0c59e72218215c68be5d70b8f372de
Anshit Verma <anshit1223@gmail.com>
2025-09-07 23:34:22 +0530

    Add Disk Usage

---
+++ disk_usage_check.py
@@ -0,0 +1,11 @@
+#!/bin/python
+import shutil
+total,used,free = shutil.disk_usage("/")
+
+def convert_bytes(bytes):
+    return round(bytes / (2**30),2)
+
+print("📦 Disk Usage for '/'")
+print(f"Total: {convert_bytes(total)} GB")
+print(f"Used:  {convert_bytes(used)} GB")
+print(f"Free:  {convert_bytes(free)} GB")
