4c61c5979c989981c1acf7c929177696b52468be
anshit-1223 <anshit1223@gmail.com>
2025-10-07 22:31:50 +0530

    add match

---
+++ match.py
@@ -0,0 +1,17 @@
+#match in python , it is just same as switch in C
+day = int(input("Enter number between 1 to 7 : "))
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
