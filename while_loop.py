ddf96044dce4db18fbf3fd099298eaa7c26c2693
anshit-1223 <anshit1223@gmail.com>
2025-10-09 22:13:06 +0530

    else statement

--- while_loop.py
+++ while_loop.py
@@ -5,9 +5,19 @@ while i < 4:
     i=i+1
 
 #break statement 
+print("Break Statement")
 i=1
 while i < 4:
     print(i)
     if i == 2:
         break
     i=i+1
+
+#else statement
+print("Else Statement")
+i=1
+while i < 4:
+    print(i)
+    i=i+1
+else:
+    print("i is no longer less than 4")
