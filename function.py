0af04cc8d952f9388e0326ac421b87ff7c48b057
anshit-1223 <anshit1223@gmail.com>
2025-10-11 21:54:35 +0530

    arbitrary arguments

--- function.py
+++ function.py
@@ -7,3 +7,10 @@ my_function()
 def my_name(name):
     print(name)
 my_name("Anshit")
+
+
+#arbitrary arguments - when you dont no the number of arguments will be passed into your function
+def arbitrary_function(*fruits):
+    print(fruits[1])
+
+arbitrary_function("mango","apple","grapes")
