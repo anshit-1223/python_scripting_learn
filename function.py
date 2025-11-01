4347e98ca640c2b5fd21b8abee924f9ac11cf743
anshit-1223 <anshit1223@gmail.com>
2025-11-01 11:41:53 +0530

    lambda definition

--- function.py
+++ function.py
@@ -123,6 +123,9 @@ def myfunction():
 print(myfunction())
 
 #python lambda
+## A lambda function is a small anonymous function.
+#A lambda function can take any number of arguments, but can only have one expression.
+
 x = lambda a : a+10
 print(x(5))
 
