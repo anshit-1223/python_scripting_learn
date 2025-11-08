db96046fc366c8d00d82a1f939498af341dc7521
anshit-1223 <anshit1223@gmail.com>
2025-11-08 10:31:08 +0530

    code refactor

--- make_dir.py
+++ make_dir.py
@@ -9,4 +9,4 @@ if not os.path.exists(directory_name):
     os.makedirs(directory_name)
     print(f"Directory '{directory_name}' created.")
 else:
-    print(f"Directory '{directory_name}' already exists.")
+    print(f"Directory '{directory_name}' Already Exists.")
